aa77c0815f3efbe0c10a73fe6a4e997d9a58123f
dcai2018 <OpsDev@CitGuest-MBP.lan>
2018-06-14 15:12:16 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,7 @@
+Sample Ionic project to authenticate with MS AAD via its ADAL Cordova Plugin. Also use MS Graph API to fetch data from Sharepoint Online.
+
+This can only run in device or simulator.
+
+Command to run in simulator:
+ionic cordova emulate ios (or android)
+
---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+
+import { MSAdal } from '@ionic-native/ms-adal';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    HttpClient,
+    MSAdal,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
